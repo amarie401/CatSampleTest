fd85153bf1abf810d5f7d2341f97a465bce51049
Amiee Lo <amlo@microsoft.com>
2020-03-16 17:27:50 -0700

    initial commit

---
+++ CatApi/Controllers/CatsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatApi.Controllers
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CatsController : ControllerBase
+    {
+
+        public static List<Cat> cats = new List<Cat>();
+
+        static CatsController()
+        {
+            cats.Add(new Cat { Id = 1, Name = "Amiee" });
+            cats.Add(new Cat { Id = 2, Name = "Ryan" });
+            cats.Add(new Cat { Id = 3, Name = "Justin" });
+            cats.Add(new Cat { Id = 4, Name = "David" });
+            cats.Add(new Cat { Id = 5, Name = "Glenn" });
+        }
+
+        [HttpGet("{id}")]
+        public Cat GetCat(int id)
+        {
+                return cats.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public string AddCat(Cat cat)
+        {
+            cats.Add(cat);
+            return $"Cats added! Now we have {cats.Count} cats!";
+        }
+
+        [HttpGet]
+        public List<Cat> GetCats()
+        {
+            return cats;
+        }
+
+    }
+}
+
---
+++ CatConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace CatConsole
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Cat(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        async static Task Main()
+        {
+            client.BaseAddress = new Uri("http://localhost:65470/");
+
+            var media = new MediaTypeWithQualityHeaderValue("application/json");
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(media);
+
+            try
+            {
+                var cat = new Cat(6, "Charles");
+                
+                var message = AddCat(cat);
+
+                Console.WriteLine($"Create: {message}");
+
+                var listOfCats = await ListCats();
+              
+                foreach (var cats in listOfCats)
+                {
+                    Console.WriteLine($"Here are your cats: {cats.Name}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string AddCat(Cat cat)
+        {
+            var action = "/cats";
+            var request = client.PostAsJsonAsync(action, cat);
+
+            var response = request.Result.Content.ReadAsStringAsync();
+
+            return response.Result;
+        }
+
+        private static async Task<List<Cat>> ListCats()
+        {
+            var action = "/cats";
+            var response = await client.GetAsync(action);
+
+            var cats = await response.Content.ReadAsAsync<List<Cat>>();
+
+            return cats;
+        }
+
+    }
+}
+
